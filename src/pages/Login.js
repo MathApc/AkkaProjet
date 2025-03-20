5c696f4874fe0a294578fcf485e1a76b34542eff
lnolland <127619531+lnolland@users.noreply.github.com>
2025-03-20 21:46:38 +0100

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";  // Ajout de l'import
+import { useAuth } from "../context/AuthContext";
 import styles from "./Login.module.css";
 
 const Login = () => {
